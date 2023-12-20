48d90cbedf6132bd98a7d5f193f5f62e3326e0df
Emily Chew <emilychewsh@gmail.com>
2023-12-20 13:30:38 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = "bob"
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+const leastFavouriteCustomer = "x"
+function changeLeastFavoriteCustomer(){
+    leastFavouriteCustomer = "flanders"
+}
+console.log(leastFavouriteCustomer)
+console.log(changeLeastFavoriteCustomer())
